c53c8344a599504d4fd6746c561330e5598b0033
NG YU YAN <22015056@myrp.edu.sg>
2023-06-07 15:55:14 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22015056/simpletodo/MainActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.c346.id22015056.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNewTodo;
+    Button btnAdd;
+    Button btnClear;
+
+    ListView lvTodo;
+
+    ArrayList<String> alTodo;
+    ArrayAdapter<String> aaTodo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etNewTodo= findViewById(R.id.editNewTodo);
+        btnAdd = findViewById(R.id.button2);
+        btnClear = findViewById(R.id.button3);
+        lvTodo = findViewById(R.id.listViewTodo);
+
+        alTodo = new ArrayList<>();
+        aaTodo = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTodo);
+        lvTodo.setAdapter(aaTodo);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newTodo = etNewTodo.getText().toString();
+                alTodo.add(newTodo);
+                aaTodo.notifyDataSetChanged();
+                etNewTodo.setText(null);
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alTodo.clear();
+                aaTodo.notifyDataSetChanged();
+            }
+        });
+    }
+}
